f6c635f727acb4e3e255694dd71c5c6c6ccfb400
mariaMerethe <borglundmaria@gmail.com>
2025-03-21 10:51:16 +0100

    Första commit - Setup med Tailwind och DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-700'>Hello Tailwindcss with react vite</h1>
+    </>
+  )
+}
+
+export default App
