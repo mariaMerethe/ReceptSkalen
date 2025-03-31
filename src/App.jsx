eac453412c46767c47ec92e93e684d0350c341ef
mariaMerethe <borglundmaria@gmail.com>
2025-03-31 16:30:04 +0200

    fixade yttre paddingen som försvann från 'hem'-sidan, nu ska jag ordna med layout i 'favorit'-sidan

--- src/App.jsx
+++ src/App.jsx
@@ -89,24 +89,26 @@ useEffect(() => {
 
         <Routes>
           <Route path="/" element={
-            <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
-              {/* vänster kolumn */}
-              <div className='lg:col-span-2 space-y-6'>
-                <SearchComponent onSearch={setSearchTerm} />
-                <ResultsComponent
-                  recipes={recipes}
-                  error={error}
-                  loading={loading}
-                  onSelectedMeal={handleSelectedMeal}
-                  searchTerm={searchTerm}
-                  favorites={favorites}
-                  toggleFavorite={toggleFavorite}
-                />
-              </div>
+            <div className="max-w-6xl mx-auto px-6">
+              <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
+                {/* vänster kolumn */}
+                <div className='lg:col-span-2 space-y-6'>
+                  <SearchComponent onSearch={setSearchTerm} />
+                  <ResultsComponent
+                    recipes={recipes}
+                    error={error}
+                    loading={loading}
+                    onSelectedMeal={handleSelectedMeal}
+                    searchTerm={searchTerm}
+                    favorites={favorites}
+                    toggleFavorite={toggleFavorite}
+                  />
+                </div>
 
-              {/* höger kolumn */}
-              <div>
-                {selectedMeal && <MealDetailComponent meal={selectedMeal} />}
+                {/* höger kolumn */}
+                <div>
+                  {selectedMeal && <MealDetailComponent meal={selectedMeal} />}
+                </div>
               </div>
             </div>
           } />
